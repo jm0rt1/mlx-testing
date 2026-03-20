f248abcfc5742dda67586c17fa21b72538ab156d
Jamie Mortensen <16mortensenj@gmail.com>
2026-03-19 20:50:27 -0400

    Initial Commit

---
+++ mlx-testing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mlx-testing
+//
+//  Created by James Mortensen on 3/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mlx-testing/mlx_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlx_testingApp.swift
+//  mlx-testing
+//
+//  Created by James Mortensen on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct mlx_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
